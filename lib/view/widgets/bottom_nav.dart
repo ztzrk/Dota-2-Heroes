ff332081bba903f0b9b8a7bac150433304a9e9f8
ztzrk <ralif877@gmail.com>
2024-05-13 16:10:54 +0700

    fix: navigation

--- lib/view/widgets/bottom_nav.dart
+++ lib/view/widgets/bottom_nav.dart
@@ -10,50 +10,61 @@ class MyBottomNavigationBar extends StatelessWidget {
   });
 
   void _onItemTapped(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        Navigator.pushNamed(context, '/home');
-        break;
-      case 1:
-        Navigator.pushNamed(context, '/team');
-        break;
-      case 2:
-        Navigator.pushNamed(context, '/player');
-        break;
-      case 3:
-        Navigator.pushNamed(context, '/fav');
-        break;
+    if (index != currentIndex) {
+      switch (index) {
+        case 0:
+          Navigator.pushReplacementNamed(context, '/home');
+          break;
+        case 1:
+          Navigator.pushReplacementNamed(context, '/team');
+          break;
+        case 2:
+          Navigator.pushReplacementNamed(context, '/player');
+          break;
+        case 3:
+          Navigator.pushReplacementNamed(context, '/fav');
+          break;
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle_rounded),
-          label: 'Heroes',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group),
-          label: 'Team',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Player',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
-          label: 'Favorite',
-        ),
-      ],
-      currentIndex: currentIndex,
-      selectedItemColor: AppColors.fourth,
-      unselectedItemColor: AppColors.third,
-      backgroundColor: Colors.amber,
-      onTap: (index) {
-        _onItemTapped(context, index);
+    return WillPopScope(
+      onWillPop: () async {
+        if (currentIndex != 0) {
+          Navigator.pushReplacementNamed(context, '/home');
+          return false;
+        }
+        return true;
       },
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Heroes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Team',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Player',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+        currentIndex: currentIndex,
+        selectedItemColor: AppColors.fourth,
+        unselectedItemColor: AppColors.third,
+        backgroundColor: Colors.amber,
+        onTap: (index) {
+          _onItemTapped(context, index);
+        },
+      ),
     );
   }
 }
